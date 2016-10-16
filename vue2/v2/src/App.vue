57aabf475abc8ccf7f6d1699a431206c8a87e859
whatlife <121046875@qq.com>
2016-10-17 07:47:44 +0800

    vue 2 example demo

---
+++ vue2/v2/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+      
+    <firstcomponent></firstcomponent>
+    <ul>
+    <li><router-link to='/first'>点我跳到 第一页</router-link></li>
+    <li><router-link to='/second'>点我跳到 第二页</router-link></li>
+</ul>
+  </div>
+</template>
+
+<script>
+import firstcomponent from './component/firstcomponent.vue';
+import secondcomponent from './component/secondcomponent.vue';
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue!'
+    }
+  },
+
+  components: { firstcomponent, secondcomponent }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ vue2/v2/src/component/firstcomponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="firstcomponent">
+        <h1>i am title!</h1>
+        <a href="##">written by {{ author }}</a>
+    </div>
+
+</template>
+
+<script>
+    export default{
+        data() {
+            return {
+                author: 'i am the written!'
+            }
+        }
+    }
+</script>
+
+<style>
+    
+</style>
